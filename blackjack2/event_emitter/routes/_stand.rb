9f64813089bf17c0ca658f0f224ff4a6f016e771
3mpirical <3mpirical10510@gmail.com>
2019-02-16 21:47:17 -0700

    initial 'stand' command featues added

--- blackjack2/event_emitter/routes/_stand.rb
+++ blackjack2/event_emitter/routes/_stand.rb
@@ -6,7 +6,42 @@ bj_stand = -> {
             return nil
         end
 
-        p "here in stand"
+        def reset_game_state
+            BJ_State.reset_bet()
+            BJ_State.reset_cards()
+            BJ_Model.reset_deck()
+            sleep(5)
+            BJ_View.make_bet_display()
+        end
+
+        while(true) 
+            BJ_State.add_dealer_card(BJ_Model.remove_card())
+            dealer_total = BJ_State.dealer_total()
+            player_total = BJ_State.player_total()
+
+            if(dealer_total > 21)
+                BJ_Model.add_player_money(BJ_State.get_bet() * 2)
+                BJ_View.main_game_display()
+                p "!! ...PLAYER WINS... !!"
+                reset_game_state()
+                break
+            elsif(dealer_total > player_total)
+                BJ_View.main_game_display()
+                p "!! ...DEALER WINS... !!"
+                reset_game_state()
+                break
+            elsif(dealer_total == player_total)
+                BJ_Model.add_player_money(BJ_State.get_bet())
+                BJ_View.main_game_display()
+                p "!! ...PUSH (TIE)... !!"
+                reset_game_state()
+                break
+            else
+                puts "\n:*:*:*: Dealers Turn :*:*:*:\n\n"
+                sleep(2)
+                BJ_View.main_game_display()
+            end
+        end
     end
 
 
