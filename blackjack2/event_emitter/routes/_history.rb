7bff3f1666a102a326be92c62a6c390ac2c349b3
3mpirical <3mpirical10510@gmail.com>
2019-02-17 18:02:14 -0700

    history route now calls history_display

---
+++ blackjack2/event_emitter/routes/_history.rb
@@ -0,0 +1,15 @@
+bj_history = -> {
+    ##### ABSTRACTED FUNCS #####
+    def handle_history
+        if(BJ_State.is_playing?())
+            BJ_View.stop_game_first()
+            return nil
+        end
+        BJ_View.handle_history()
+    end
+
+    ##### EVENT LISTENERS #####
+    BJ_Emitter.add("history") { handle_history() }
+    BJ_Emitter.add("his") { handle_history() }
+    BJ_Emitter.add("-his") { handle_history() }
+}.()
