7548ec96dc38d6513aa3fd9313ec0016d5c3f49f
3mpirical <3mpirical10510@gmail.com>
2019-02-18 12:44:41 -0700

    added validation for history command, it will no longer display if user has no history

--- blackjack2/event_emitter/routes/_history.rb
+++ blackjack2/event_emitter/routes/_history.rb
@@ -4,6 +4,9 @@ bj_history = -> {
         if(BJ_State.is_playing?())
             BJ_View.stop_game_first()
             return nil
+        elsif(BJ_Model.get_history().length == 0)
+            puts "\n:*:*:*::*:*:* You Haven't Played Yet :*:*:*:*:*:*:"
+            return nil
         end
         BJ_View.history_display()
     end
